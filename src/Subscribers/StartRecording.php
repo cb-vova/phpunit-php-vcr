4637acefacf7e7c6e22b3d83eebfd56fad5c006f
Volodymyr Stolyarchuk <volodymyr.stolyarchuk@cloudbeds.com>
2025-07-15 15:01:32 +0300

    feat: separate cassettes per dataProvider cases

--- src/Subscribers/StartRecording.php
+++ src/Subscribers/StartRecording.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Angelov\PHPUnitPHPVcr\Subscribers;
 
+use Angelov\PHPUnitPHPVcr\UseCassette;
+use Angelov\PHPUnitPHPVcr\Values\TestCaseParameters;
 use PHPUnit\Event\Test\Prepared;
 use PHPUnit\Event\Test\PreparedSubscriber;
 use VCR\VCR;
@@ -20,10 +22,34 @@ class StartRecording implements PreparedSubscriber
             return;
         }
 
-        $cassetteName = $this->getCassetteName($test);
-        assert($cassetteName !== null);
+        $testCaseCassetteParameters = $this->getTestCaseCassetteParameters($test);
+        assert($testCaseCassetteParameters instanceof TestCaseParameters);
+
+        if ($testCaseCassetteParameters->case !== null) {
+            $cassetteName = $this->makeCassetteNameForCase(
+                case: $testCaseCassetteParameters->case,
+                cassette: $testCaseCassetteParameters->cassette,
+            );
+        } else {
+            $cassetteName = $testCaseCassetteParameters->cassette->name;
+        }
 
         VCR::turnOn();
         VCR::insertCassette($cassetteName);
+    }
+
+    private function makeCassetteNameForCase(string $case, UseCassette $cassette): string
+    {
+        if (!$cassette->separateCassettePerCase) {
+            return $cassette->name;
+        }
+        $cassetteNameParts = explode('.', $cassette->name);
+        $cassetteSuffix = $cassette->groupCaseFilesInDirectory ? '/' . $case : '-' . $case;
+        if (count($cassetteNameParts) === 1) {
+            //cassette name does not contain a dot, so we can use it as is
+            return $cassette->name . $cassetteSuffix;
+        }
+        $ext = array_pop($cassetteNameParts);
+        return implode('.', $cassetteNameParts) . $cassetteSuffix . '.' . $ext;
     }
 }
